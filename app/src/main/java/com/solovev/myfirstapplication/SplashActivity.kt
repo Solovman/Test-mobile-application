dc32db9d98a93bc4597c72edf09f45283f9e71f0
Илья Соловьев <solovevilya3101@gmail.com>
2023-12-18 17:50:44 +0200

    Adding loading animation

--- app/src/main/java/com/solovev/myfirstapplication/SplashActivity.kt
+++ app/src/main/java/com/solovev/myfirstapplication/SplashActivity.kt
@@ -1,13 +1,28 @@
 package com.solovev.myfirstapplication
 
+import android.content.Intent
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 
 //Тут будет происходить выбор, какое именно активити будет открыто
 //при запуске приложения в том или ином сценарии
 class SplashActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_splash)
+
+        progressBar = findViewById(R.id.progress_bar)
+
+        // Логика для перехода на RegistrationActivity.
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
     }
 }
